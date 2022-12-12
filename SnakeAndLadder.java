38f9cf465545228c2cf300e308c250e2ff4df373
Saipavanganesh <pavanganeshms@gmail.com>
2022-12-13 03:38:25 +0530

    Plays until player reaches 100 position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -6,26 +6,27 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Game starts....Position:0");
 
-        double playerOnePosition=0;
+        double playerOnePosition=0.0;
 
-        double dice=(Math.floor(Math.random()*10)%6)+1.0;
-        System.out.println("Dice:"+dice);
-        double option=(Math.floor(Math.random()*10)%3);
-        System.out.println("Option:"+option);
+        while(playerOnePosition<=100.0){
+            double dice=(Math.floor(Math.random()*10)%6)+1.0;
+            double option=(Math.floor(Math.random()*10)%3);
 
-        switch(Double.toString(option)){
-            case "0.0":
-                System.out.println("No Play");
-                break;
-            case "1.0":
-                System.out.println("Ladder");
-                playerOnePosition=playerOnePosition+dice;
-                break;
-            default:
-                System.out.println("Snake");
-                playerOnePosition=playerOnePosition-dice;
-                break;
+            switch(Double.toString(option)){
+                case "0.0":
+                    System.out.println("No Play");
+                    break;
+                case "1.0":
+                    System.out.println("Ladder");
+                    playerOnePosition=playerOnePosition+dice;
+                    break;
+                default:
+                    System.out.println("Snake");
+                    playerOnePosition=playerOnePosition-dice;
+                    break;
+            }
         }
+
         System.out.println("Player one position is "+playerOnePosition);
     }
 }
