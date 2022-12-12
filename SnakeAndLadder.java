cf5938a306f7c4435bf39b4e905439a3016b752f
Saipavanganesh <pavanganeshms@gmail.com>
2022-12-13 02:53:07 +0530

    Game starts at position 0

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,7 +4,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        int playerOnePosition=0;
         System.out.println("Game starts....Position:0");
     }
 }
