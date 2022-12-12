300f254b93f08eb1b3d5f6fc378c1debe9c4a068
Saipavanganesh <pavanganeshms@gmail.com>
2022-12-13 02:50:53 +0530

    First commit

---
+++ SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.snakeandladder;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        int playerOnePosition=0;
+        System.out.println("Game starts....Position:0");
+    }
+}
