8656b373a132c8b8f2f18151e3f1aea48d3d94a6
Saipavanganesh <pavanganeshms@gmail.com>
2022-12-13 03:01:37 +0530

    Added random value in dice between 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,9 +1,12 @@
 package com.snakeandladder;
 
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
+import java.lang.Math;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Game starts....Position:0");
+
+        int playerOnePosition=0;
+        double dice=(Math.floor(Math.random()*10)%6)+1.0;
     }
 }
