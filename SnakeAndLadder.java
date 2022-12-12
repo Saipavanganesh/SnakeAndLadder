5408804b63cd63c1b6ff522737181cc854fa5cf3
Saipavanganesh <pavanganeshms@gmail.com>
2022-12-13 04:18:36 +0530

    Displays count of dice and current position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,28 +2,31 @@ package com.snakeandladder;
 
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 import java.lang.Math;
+import java.util.ArrayList;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Game starts....Position:0");
 
         double playerOnePosition=0.0;
+        int count=0;
 
         while(playerOnePosition<100.0){
+            count++;
             double dice=(Math.floor(Math.random()*10)%6)+1.0;
             double option=(Math.floor(Math.random()*10)%3);
 
             switch(Double.toString(option)){
                 case "0.0":
-                    System.out.println("No Play");
+                    System.out.println("No Play. Current position is:"+playerOnePosition);
                     break;
                 case "1.0":
-                    System.out.println("Ladder");
+                    System.out.println("Ladder. Curent position is:"+(playerOnePosition+dice));
                     if(playerOnePosition+dice<=100){
                         playerOnePosition=playerOnePosition+dice;
                     }
                     break;
                 default:
-                    System.out.println("Snake");
+                    System.out.println("Snake. Current position is:"+(playerOnePosition-dice));
                     if (playerOnePosition-dice<0.0) {
                         playerOnePosition=0.0;
                     }
@@ -34,6 +37,7 @@ public class SnakeAndLadder {
             }
         }
 
+        System.out.println("Dice was played "+count+" times");
         System.out.println("Player one position is "+playerOnePosition);
     }
 }
